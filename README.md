44941e215c7b79911cc303e6303e093e0ee8803a
Flyiso <dexterpetterssonspang@gmail.com>
2025-03-24 00:40:01 +0100

    terminal, non-audio, and not yet with all features version working

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # motivationbot
 chatbot generating "motivational" quotes
+
+
+Example of what this thing currently does:
+(yes, very terminal.)
+....................................................................................................
+what do you need help with?
+the depths of despair
+....................................................................................................
+
+####################################################################################################
+
+Hey there, friend! I can totally sense that you're feeling overwhelmed and stuck in a sea of uncertainty right now. It's okay to feel that way - we all do sometimes!
+
+Here's the thing: you are not alone in this struggle. We've all been there at some point or another. And you know what? You've made it through those tough times before, and you can do it again.
+
+Listen, I want you to take a deep breath with me right now... Inhale... hold it... exhale... Feel that weight lifting off your shoulders just a little bit?
+
+Now, let's talk about something super important: YOU ARE STRONGER THAN YOU THINK! Those dark moments might feel like they're going to consume you whole, but trust me, you have the power to rise above them.
+
+Remember those times when life was tough, and you thought all hope was lost? But then something miraculous happened - you found a way to push through, to adapt, or to find that tiny glimmer of light in the darkness?
+
+That's what I want you to tap into right now. That inner strength, resilience, and determination are still there, my friend! You've just got to dig deep and remember those moments when you felt like giving up, but you didn't.
+
+You know why? Because YOU'RE A WARRIOR! You're a champion who's been in the trenches before and has come out on top. And I believe with all my heart that you can do it again!
+
+So here's what I want you to do: take small steps forward, one at a time. Don't worry about the whole picture; just focus on taking that next step. You got this! When you start moving forward, even if it's just an inch, you'll begin to feel the momentum building.
+
+And when those dark thoughts creep in (because they will), don't fight them. Just acknowledge them, breathe through them, and remind yourself of your strength. You are capable of overcoming anything that comes your way!
+
+You're not alone, my friend. I'm right here with you, cheering you on every step of the way. And I know that with time, patience, and a little bit of faith in yourself, you'll rise above those depths of despair and shine brighter than ever before!
+
+Keep going, champ! You got this!
+
+####################################################################################################
+
+ARE YOU KIDDING ME?! YOU'RE COMPLAINING ABOUT STRUGGLING WITH THE DEPTHS OF DESPAIR?! YOU THINK YOU'RE SOME KIND OF SPECIAL SNOWFLAKE WHO CAN'T HANDLE A LITTLE BIT OF SUFFERING?!
+
+NEWSFLASH, CHUMP: EVERYONE ELSE HAS BEEN WHERE YOU ARE AND CAME OUT ON TOP! BUT INSTEAD OF LEARNING FROM THEIR EXPERIENCES, YOU'RE TOO BUSY WHINING ABOUT HOW HARD IT IS!
+
+YOU KNOW WHAT'S REALLY HARD?! IT'S GETTING BACK UP AFTER BEING KNOCKED DOWN! IT'S PUSHING THROUGH THE PAIN AND THE FEAR AND THE DOUBT! IT'S NOT SOME PATHETIC EXCUSE FOR WHY YOU CAN'T ACHIEVE YOUR GOALS!
+
+SO HERE'S WHAT I WANT FROM YOU: I WANT YOU TO STOP WHINING AND START GRINDING! I WANT YOU TO GET UP, DUST YOURSELF OFF, AND KEEP MOVING FORWARD! I DON'T CARE IF IT TAKES EVERY OUNCE OF STRENGTH YOU HAVE, BUT I EXPECT YOU TO FIND A WAY TO GET THROUGH THIS!
+
+YOU'RE NOT A VICTIM, YOU'RE A WARRIOR! YOU'RE NOT SOME WEAKLING WHO CAN'T HANDLE THE PRESSURE! YOU'RE A FIGHTER, AND IT'S TIME YOU STARTED ACTING LIKE ONE!
+
+SO HERE'S YOUR ASSIGNMENT: I WANT YOU TO TAKE 10 MINUTES TODAY TO WRITE DOWN EVERYTHING THAT'S WRONG WITH YOUR LIFE. I WANT YOU TO LIST ALL THE WAYS YOU FEEL LIKE GIVING UP, ALL THE REASONS WHY YOU CAN'T SUCCEED! AND THEN, I WANT YOU TO REREAD IT ALL, AND FOR EACH POINT, I WANT YOU TO WRITE A RESPONSE THAT CHALLENGES THAT NEGATIVITY!
+
+ARE YOU READY TO TAKE ON THIS ASSIGNMENT?! ARE YOU READY TO STOP MAKING EXCUSES AND START MAKING PROGRESS?!
+
+IF SO, THEN LET'S GET TO WORK!
+
+####################################################################################################
--- main.py
+++ main.py
@@ -2,22 +2,25 @@ from ollama import create, chat
 
 class MotiBot:
     def __init__(self, 
-                 bot_name: str = 'Rolf', toughness:int = 75,
+                 bot_name: str = 'Rolf',
+                 toughness:int = 75, intensity: int = 80,
                  meanness:int = 40, seriousness:int = 20,
                  critic_level:int = 90, user_belief:int = 90):
         self.bot_name = bot_name
         self.toughness = self._get_toughness(toughness)
+        self.intensity = self._get_intensity(intensity)
         self.meanness = self._get_meanness(meanness)
         self.seriousness = self._get_seriousness(seriousness)
         self.critic_level = self._get_critic_level(critic_level)
         self.user_belief = self._get_user_beief(user_belief)
         self.personality = self._get_personality()
-        self.system_prompt = /
-        f'You are a coach who deliver motivational speaches.{self.personality}'
+        self.system_prompt = \
+        f'You are a coach who deliver motivational speaches through text. {self.personality}'
+        #print(f'{"*"*20}\n Model personality prompt:\n{self.system_prompt}\n{"*"*20}\n')
         self.model = self._create_bot()
     
     def _get_personality(self):
-        return f'{self.toughness} {self.meanness} {self.seriousness} {self.critic_level} {self.user_belief}'
+        return f'{self.toughness} {self.intensity} {self.meanness} {self.seriousness} {self.critic_level} {self.user_belief}'
     
     def _get_toughness(self, q_level: int) -> str:
         """
@@ -25,12 +28,26 @@ class MotiBot:
 
         :param q_level: int- scale of 0-100 from opposite to maximum. 
         """
-        if q_level >= 50:
+        if q_level <= 50:
             quality = 'patient'
-        if q_level <= 49:
+        if q_level >= 49:
             quality = 'tough'
         level = max([q_level, 50]) - min([q_level, 50])
-        q_string = f'Personality-wise you are {self._get_quality_quantity(level)} {quality}.'
+        q_string = f'Personality-wise you are {self._get_quality_quantity(level)} {quality},'
+        return q_string
+    
+    def _get_intensity(self, q_level: int) -> str:
+        """
+        How tough or patient the assistant should be.
+
+        :param q_level: int- scale of 0-100 from opposite to maximum. 
+        """
+        if q_level >= 50:
+            quality = 'intense and yell sometimes'
+        if q_level <= 49:
+            quality = 'calm'
+        level = max([q_level, 50]) - min([q_level, 50])
+        q_string = f'you are {self._get_quality_quantity(level)} {quality},'
         return q_string
 
     def _get_meanness(self, q_level: int) -> str:
@@ -39,12 +56,12 @@ class MotiBot:
 
         :param q_level: int- scale of 0-99 from opposite to maximum. 
         """
-        if q_level >= 50:
+        if q_level <= 50:
             quality = 'friendly'
-        if q_level <= 49:
+        if q_level >= 49:
             quality = 'mean'
         level = max([q_level, 50]) - min([q_level, 50])
-        q_string = f'You are also {self._get_quality_quantity(level)} {quality}.'
+        q_string = f'and are also {self._get_quality_quantity(level)} {quality}.'
         return q_string
     
     def _get_seriousness(self, q_level: int) -> str:
@@ -53,9 +70,9 @@ class MotiBot:
 
         :param q_level: int- scale of 0-100 from opposite to maximum. 
         """
-        if q_level >= 50:
+        if q_level <= 50:
             quality = 'playful'
-        if q_level <= 49:
+        if q_level >= 49:
             quality = 'serious'
         level = max([q_level, 50]) - min([q_level, 50])
         q_string = f'Your attitude is mainly {self._get_quality_quantity(level)} {quality}.'
@@ -67,9 +84,9 @@ class MotiBot:
 
         :param q_level: int- scale of 0-100 from opposite to maximum. 
         """
-        if q_level >= 50:
+        if q_level <= 50:
             quality = 'positive'
-        if q_level <= 49:
+        if q_level >= 49:
             quality = 'critical'
         level = max([q_level, 50]) - min([q_level, 50])
         q_string = f'Your attitude to the users achivements are primarily {self._get_quality_quantity(level)} {quality} and'
@@ -120,7 +137,44 @@ class MotiBot:
         motivation = chat(model=self.bot_name, messages=[
             {
                 'role': 'user',
-                'content': f'give a motivational speash to the user who struggles with {mot_theme}'
+                'content': f'give a motivational speach to the user who struggles with {mot_theme}.'
             }
         ])
-        return motivation
+        return motivation.message.content
+
+    def get_audio_motivation(self, mot_theme):
+        print(self.get_motivation(mot_theme))
+
+print('.'*100)
+problem = input('what do you need help with?\n')
+print('.'*100)
+print('')
+
+b_name = 'jon'
+toughness = 10
+intensity = 10
+meanness = 10
+seriousness = 10
+critic_level = 10
+user_belief = 95
+
+print('#'*100)
+print('')
+custom_bot = MotiBot(b_name, toughness, intensity, meanness, seriousness, critic_level, user_belief)
+custom_bot.get_audio_motivation(problem)
+
+b_name = 'noj'
+toughness = 90
+intensity = 95
+meanness = 95
+seriousness = 90
+critic_level = 95
+user_belief = 5
+
+print('')
+print('#'*100)
+print('')
+custom_bot = MotiBot(b_name, toughness, intensity, meanness, seriousness, critic_level, user_belief)
+custom_bot.get_audio_motivation(problem)
+print('')
+print('#'*100)
